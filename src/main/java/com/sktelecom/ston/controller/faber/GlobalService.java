0bfe30e4d1a7e73b771114c7306e5c2b8576a6d3
Ethan Sung <baegjae@gmail.com>
2021-04-27 16:43:02 +0900

    Don't use actual encoded jpeg image v1
    Signed-off-by: Ethan Sung <baegjae@gmail.com>

--- src/main/java/com/sktelecom/ston/controller/faber/GlobalService.java
+++ src/main/java/com/sktelecom/ston/controller/faber/GlobalService.java
@@ -322,10 +322,11 @@ public class GlobalService {
         // uncomment below if you want to get requested credential definition id from alice
         //String requestedCredDefId = JsonPath.read(credentialProposal, "$.cred_def_id");
 
-        String encodedImage = "";
-        try {
-            encodedImage = encodeFileToBase64Binary(photoFileName);
-        } catch (Exception e) { e.printStackTrace(); }
+        String encodedImage = "base64EncodedJpegImage";
+        // uncomment below if you want to use actual encoded jpeg image
+        //try {
+        //    encodedImage = encodeFileToBase64Binary(photoFileName);
+        //} catch (Exception e) { e.printStackTrace(); }
         String body = JsonPath.parse("{" +
                 "  connection_id: '" + connectionId + "'," +
                 "  cred_def_id: '" + credDefId + "'," +
